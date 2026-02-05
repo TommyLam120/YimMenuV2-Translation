6b1cafaf61584f027e871668bd66be59f51ab75e
Tommy <94686346+TommyLam120@users.noreply.github.com>
2026-02-05 15:27:57 +0800

    update

--- src/game/frontend/submenus/Player/Troll.cpp
+++ src/game/frontend/submenus/Player/Troll.cpp
@@ -1,25 +1,27 @@
 #include "Troll.hpp"
+#include "core/localization/Translator.hpp"
+#define TR(key) YimMenu::Translator::Get(key).c_str()
 
 namespace YimMenu::Submenus
 {
 	std::shared_ptr<Category> BuildTrollMenu()
 	{
-		auto menu = std::make_shared<Category>("Troll");
+		auto menu = std::make_shared<Category>(TR("Troll"));
 
-		auto general = std::make_shared<Group>("General");
-		auto bounty = std::make_shared<Group>("Bounty", 1);
-		auto vehicle = std::make_shared<Group>("Vehicle");
+		auto general = std::make_shared<Group>(TR("General"));
+		auto bounty = std::make_shared<Group>(TR("Bounty"), 1);
+		auto vehicle = std::make_shared<Group>(TR("Vehicle"));
 
-		general->AddItem(std::make_shared<PlayerCommandItem>("sendsext"_J));
-		general->AddItem(std::make_shared<PlayerCommandItem>("sendphantomcar"_J));
-		general->AddItem(std::make_shared<PlayerCommandItem>("sendgooch"_J));
-		general->AddItem(std::make_shared<PlayerCommandItem>("giveplayerallweapon"_J));
+		general->AddItem(std::make_shared<PlayerCommandItem>("sendsext"_J,TR("Send Sext")));
+		general->AddItem(std::make_shared<PlayerCommandItem>("sendphantomcar"_J,TR("Send Phantom Car")));
+		general->AddItem(std::make_shared<PlayerCommandItem>("sendgooch"_J,TR("Send Gooch")));
+		general->AddItem(std::make_shared<PlayerCommandItem>("giveplayerallweapon"_J,TR("Give Player All Weapon")));
 
-		bounty->AddItem(std::make_shared<IntCommandItem>("bountyamount"_J, "Amount"));
-		bounty->AddItem(std::make_shared<BoolCommandItem>("anonymousbounty"_J, "Anonymous"));
-		bounty->AddItem(std::make_shared<PlayerCommandItem>("setbounty"_J));
+		bounty->AddItem(std::make_shared<IntCommandItem>("bountyamount"_J, TR("Amount")));
+		bounty->AddItem(std::make_shared<BoolCommandItem>("anonymousbounty"_J, TR("Anonymous")));
+		bounty->AddItem(std::make_shared<PlayerCommandItem>("setbounty"_J,TR("Set Bounty")));
 
-		vehicle->AddItem(std::make_shared<PlayerCommandItem>("deleteveh"_J));
+		vehicle->AddItem(std::make_shared<PlayerCommandItem>("deleteveh"_J,TR("Delete Vehicle")));
 
 		menu->AddItem(general);
 		menu->AddItem(bounty);
