2e9eb23634aedff9e323d2858335cb0b79132aae
Tommy <94686346+TommyLam120@users.noreply.github.com>
2026-02-06 12:59:44 +0800

    update

--- src/game/frontend/submenus/Recovery/HeistModifier.cpp
+++ src/game/frontend/submenus/Recovery/HeistModifier.cpp
@@ -3,13 +3,15 @@
 #include "Heist/DiamondCasinoHeist.hpp"
 #include "Heist/CayoPericoHeist.hpp"
 #include "Heist/DoomsdayHeist.hpp"
+#include "core/localization/Translator.hpp"
+#define TR(key) YimMenu::Translator::Get(key).c_str()
 
 namespace YimMenu::Submenus
 {
 	std::shared_ptr<Category> BuildHeistModifierMenu()
 	{
-		auto menu        = std::make_shared<Category>("Heists");
-		auto heistTabBar = std::make_shared<TabBarItem>("Heist Modifier");
+		auto menu        = std::make_shared<Category>(TR("Heists"));
+		auto heistTabBar = std::make_shared<TabBarItem>(TR("Heist Modifier"));
 
 		heistTabBar->AddItem(RenderApartmentHeistMenu());
 		heistTabBar->AddItem(RenderDiamondCasinoHeistMenu());
