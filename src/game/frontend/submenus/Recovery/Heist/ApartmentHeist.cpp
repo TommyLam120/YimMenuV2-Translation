97b929f6ca42ba10e75952a4c0d223e63c94aacb
Tommy <94686346+TommyLam120@users.noreply.github.com>
2026-02-06 13:10:52 +0800

    update

--- src/game/frontend/submenus/Recovery/Heist/ApartmentHeist.cpp
+++ src/game/frontend/submenus/Recovery/Heist/ApartmentHeist.cpp
@@ -1,29 +1,31 @@
 #include "ApartmentHeist.hpp"
+#include "core/localization/Translator.hpp"
+#define TR(key) YimMenu::Translator::Get(key).c_str()
 
 namespace YimMenu::Submenus
 {
 	std::shared_ptr<TabItem> RenderApartmentHeistMenu()
 	{
-		auto tab = std::make_shared<TabItem>("Apartment Heist");
+		auto tab = std::make_shared<TabItem>(TR("Apartment Heist"));
 
-		auto cuts = std::make_shared<Group>("Heist Cuts", 2);
-		auto setups = std::make_shared<Group>("Heist Setups");
-		auto misc = std::make_shared<Group>("Misc", 1);
+		auto cuts = std::make_shared<Group>(TR("Heist Cuts"), 2);
+		auto setups = std::make_shared<Group>(TR("Heist Setups"));
+		auto misc = std::make_shared<Group>(TR("Misc"), 1);
 
-		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut1"_J));
-		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut3"_J));
-		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut2"_J));
-		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut4"_J));
-		cuts->AddItem(std::make_shared<CommandItem>("apartmentheistforceready"_J));
-		cuts->AddItem(std::make_shared<CommandItem>("apartmentheistsetcuts"_J));
+		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut1"_J,TR("Player 1")));
+		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut3"_J,TR("Player 3")));
+		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut2"_J, TR("Player 2")));
+		cuts->AddItem(std::make_shared<IntCommandItem>("apartmentheistcut4"_J,TR("Player 4")));
+		cuts->AddItem(std::make_shared<CommandItem>("apartmentheistforceready"_J,TR("Force Ready")));
+		cuts->AddItem(std::make_shared<CommandItem>("apartmentheistsetcuts"_J,TR("Set Cuts")));
 
-		setups->AddItem(std::make_shared<CommandItem>("apartmentheistsetup"_J));
+		setups->AddItem(std::make_shared<CommandItem>("apartmentheistsetup"_J,TR("Setup")));
 
-		misc->AddItem(std::make_shared<CommandItem>("apartmentheistskiphacking"_J));
-		misc->AddItem(std::make_shared<CommandItem>("apartmentheistskipdrilling"_J));
-		misc->AddItem(std::make_shared<CommandItem>("apartmentheistskipswiping"_J));
-		misc->AddItem(std::make_shared<CommandItem>("apartmentheistinstantfinish"_J));
-		misc->AddItem(std::make_shared<CommandItem>("apartmentheistinstantfinishpacific"_J));
+		misc->AddItem(std::make_shared<CommandItem>("apartmentheistskiphacking"_J,TR("Skip Hacking")));
+		misc->AddItem(std::make_shared<CommandItem>("apartmentheistskipdrilling"_J,TR("Skip Drilling")));
+		misc->AddItem(std::make_shared<CommandItem>("apartmentheistskipswiping"_J,TR("Skip Swiping")));
+		misc->AddItem(std::make_shared<CommandItem>("apartmentheistinstantfinish"_J,TR("Instant Finish")));
+		misc->AddItem(std::make_shared<CommandItem>("apartmentheistinstantfinishpacific"_J,TR("Instant Finish (Pacific)")));
 
 		tab->AddItem(cuts);
 		tab->AddItem(setups);
