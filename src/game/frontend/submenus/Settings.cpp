e7296cecfa8bf26c02c8cde97b381a407d104123
Tommy <94686346+TommyLam120@users.noreply.github.com>
2026-02-07 11:58:31 +0800

    Add files via upload

--- src/game/frontend/submenus/Settings.cpp
+++ src/game/frontend/submenus/Settings.cpp
@@ -9,6 +9,7 @@
 #include "game/frontend/submenus/Settings/LuaScripts.hpp"
 #include "game/frontend/submenus/Settings/GUISettings.hpp"
 #include "core/localization/Translator.hpp"
+#define TR(key) YimMenu::Translator::Get(key).c_str()
 
 namespace YimMenu::Submenus
 {
@@ -47,60 +48,60 @@ namespace YimMenu::Submenus
 	#define ICON_FA_GEARS "\xef\x80\x93"
 	    Submenu::Submenu(TR("Settings"), ICON_FA_GEARS)
 	{
-		auto hotkeys = std::make_shared<Category>("Hotkeys");
-		auto gui = std::make_shared<Category>("GUI");
-		auto game = std::make_shared<Category>("Game");
+		auto hotkeys = std::make_shared<Category>(TR("Hotkeys"));
+		auto gui = std::make_shared<Category>(TR("GUI"));
+		auto game = std::make_shared<Category>(TR("Game"));
 
-		auto uiStyle = std::make_shared<Group>("UI");
-		auto playerEsp = std::make_shared<Group>("Player ESP", 10);
-		auto pedEsp = std::make_shared<Group>("Ped ESP", 10);
-		auto objectEsp = std::make_shared<Group>("Object ESP");
-		auto overlay = std::make_shared<Group>("Overlay");
-		auto chat = std::make_shared<Group>("Chat");
+		auto uiStyle = std::make_shared<Group>(TR("UI"));
+		auto playerEsp = std::make_shared<Group>(TR("Player ESP"), 10);
+		auto pedEsp = std::make_shared<Group>(TR("Ped ESP"), 10);
+		auto objectEsp = std::make_shared<Group>(TR("Object ESP"));
+		auto overlay = std::make_shared<Group>(TR("Overlay"));
+		auto chat = std::make_shared<Group>(TR("Chat"));
 
 		hotkeys->AddItem(std::make_shared<ImGuiItem>(Hotkeys));
 
 		// Players
-		uiStyle->AddItem(std::make_shared<ListCommandItem>("styleselector"_J));
+		uiStyle->AddItem(std::make_shared<ListCommandItem>("styleselector"_J,TR("Styles Elector")));
 
-		playerEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawplayers"_J));
-		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espdrawdeadplayers"_J)));
+		playerEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawplayers"_J,TR("Draw Players")));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espdrawdeadplayers"_J,TR("Draw Dead Players"))));
 
-		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espnameplayers"_J, "Player Name")));
-		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<ColorCommandItem>("namecolorplayers"_J)));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espnameplayers"_J, TR("Player Name"))));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<ColorCommandItem>("namecolorplayers"_J,TR("Player Name Color"))));
 
-		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espdistanceplayers"_J, "Player Distance")));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espdistanceplayers"_J, TR("Player Distance"))));
 
-		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espskeletonplayers"_J, "Player Skeleton")));
-		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<ColorCommandItem>("skeletoncolorplayers"_J)));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espskeletonplayers"_J, TR("Player Skeleton"))));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<ColorCommandItem>("skeletoncolorplayers"_J,TR("Player Skeleton Color"))));
 
 		// Peds
-		pedEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawpeds"_J));
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espdrawdeadpeds"_J)));
+		pedEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawpeds"_J,TR("Draw Peds")));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espdrawdeadpeds"_J,TR("Draw Dead Peds"))));
 
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espmodelspeds"_J, "Ped Hashes")));
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<ColorCommandItem>("hashcolorpeds"_J)));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espmodelspeds"_J, TR("Ped Hashes"))));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<ColorCommandItem>("hashcolorpeds"_J,TR("Ped Hashes Color"))));
 
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espnetinfopeds"_J, "Ped Net Info")));
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espscriptinfopeds"_J, "Ped Script Info")));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espnetinfopeds"_J, TR("Ped Net Info"))));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espscriptinfopeds"_J, TR("Ped Script Info"))));
 
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espdistancepeds"_J, "Ped Distance")));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espdistancepeds"_J, TR("Ped Distance"))));
 
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espskeletonpeds"_J, "Ped Skeleton")));
-		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<ColorCommandItem>("skeletoncolorpeds"_J)));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espskeletonpeds"_J, TR("Ped Skeleton"))));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<ColorCommandItem>("skeletoncolorpeds"_J,TR("Ped Skeleton Color"))));
 
-		objectEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawobjects"_J));
-		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<ColorCommandItem>("hashcolorobjects"_J)));
-		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espnetinfoobjects"_J, "Object Net Info")));
-		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espscriptinfoobjects"_J, "Object Script Info")));
+		objectEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawobjects"_J,TR("Draw Special Objects")));
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<ColorCommandItem>("hashcolorobjects"_J,TR("Draw Special Objects Color"))));
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espnetinfoobjects"_J, TR("Object Net Info"))));
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espscriptinfoobjects"_J, TR("Object Script Info"))));
 
-		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espdistanceobjects"_J, "Object Distance")));
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espdistanceobjects"_J, TR("Object Distance"))));
 
 
-		overlay->AddItem(std::make_shared<BoolCommandItem>("overlay"_J));
-		overlay->AddItem(std::make_shared<ConditionalItem>("overlay"_J, std::make_shared<BoolCommandItem>("overlayfps"_J)));
+		overlay->AddItem(std::make_shared<BoolCommandItem>("overlay"_J,TR("Overlay Enable")));
+		overlay->AddItem(std::make_shared<ConditionalItem>("overlay"_J, std::make_shared<BoolCommandItem>("overlayfps"_J,TR("Overlay Show FPS"))));
 
-		chat->AddItem(std::make_shared<BoolCommandItem>("clearchat"_J));
+		chat->AddItem(std::make_shared<BoolCommandItem>("clearchat"_J,TR("Clear Chat")));
 
 		game->AddItem(playerEsp);
 		game->AddItem(pedEsp);
