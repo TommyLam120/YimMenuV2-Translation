c71d6af30750dfb02577b005752ae6d19907c1d5
Tommy <tommylam120@gmail.com>
2026-02-11 23:21:35 +0800

    delete

--- src/core/scripting/LuaUserInterface.cpp
+++
@@ -1,9 +0,0 @@
-#include "LuaUserInterface.hpp"
-
-namespace YimMenu::Lua
-{
-	UIElementBase::UIElementBase(LuaUserInterface& interface) :
-	    m_Interface(interface)
-	{
-	}
-}
--- src/core/util/utils.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "common.hpp"
-#include "types/script/types.hpp"
-namespace Utils {
-	void ApplyForceToEntity(Entity entity, float x, float y, float z, float offX, float offY, float offZ);
-}
